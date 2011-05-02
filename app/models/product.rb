c578f4144fcf70c83bb2752078bae1f44a5c9fde
itboards@hotmail.com <itboards@hotmail.com>
2011-05-02 23:12:32 +0200

    Cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,18 @@
 class Product < ActiveRecord::Base
-	default_scope :order => 'title'
+	default_scope :order => 'title'	
+	has_many :line_items
+	
+	before_destroy :ensure_not_referenced_by_any_line_item
 	
 	validates :title, :description, :image_url, :presence => true
+	
+	private
+	def ensure_not_referenced_by_any_line_item
+		if line_items.count.zero?
+			return true
+		else
+			errors.add(:base, 'Line Items present')
+			return false
+		end
+	end	
 end
